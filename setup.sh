4c45be44d564a3caafee85b42d1ffc8ce58c1381
Hugo Sibony <hsibony@pm.me>
2026-08-16 12:11:09 +0200

    Release: bump PIN

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ set -eu
 
 # configs come from a pinned commit, not a mutable branch: the bytes you
 # get are exactly the bytes reviewed at release time
-PIN=7ed9ec6f1cc5a70cef7523f8c6443050e359ee51
+PIN=375ea1276c4f18c6d30584281bfa3352afc38dc6
 BASE="${PIE_BASE:-https://raw.githubusercontent.com/KazeTachinuu/epita-ing1-setup/$PIN}"
 AFS="${AFS_DIR:-$HOME/afs}"
 DOT="$AFS/.confs"
